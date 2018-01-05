9e23979824012d44a6542430f11d3c016d599e2e
Mithilan Sivanesan <mithilan91@gmail.com>
2018-01-04 22:23:35 -0500

    fix fileUtils package testing

--- fileUtils/fileUtils_test.go
+++ fileUtils/fileUtils_test.go
@@ -3,14 +3,19 @@ import "fmt"
 		
 
 func ExampleParseFileName(){
-	fmt.Println(parseFileName("../testfile.txt"))
+	fmt.Println(ParseFileName("../testfile.txt"))
 	//Output:
 	//testfile.txt
 }
 
 func ExampleGetSha1(){
-	fmt.Printf("%x",getSha1("../testfile.txt"))
+	fmt.Printf("%x",GetSha1("../testfile.txt"))
 	//Output:
 	//2fd4e1c67a2d28fced849ee1bb76e7391b93eb12	
 }
 
+func ExampleGetMd5(){
+	fmt.Printf("%x",GetMd5("../testfile.txt"))
+	//Output:
+	//9e107d9d372bb6826bd81d3542a419d6
+}
